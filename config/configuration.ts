412e3dfc0cddef3ec579eaef185cf6046a07488e
dm.okhrimenko <dm.okhrimenko@gmail.com>
2021-06-25 18:48:14 +0300

    ref(auth): refactoring project structure

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,17 +1,32 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    client: process.env.DB_CLIENT,
-    debug: false,
-    connection: {
-      host: process.env.DATABASE_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: parseInt(process.env.DB_PORT, 10) || 1433,
-      options: {
-        encrypt: false,
-      },
+    // For a sync connection (forRoot)
+
+    // client: process.env.DB_CLIENT,
+    // debug: false,
+    // connection: {
+    //   host: process.env.DATABASE_HOST,
+    //   user: process.env.DB_USER,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME,
+    //   port: parseInt(process.env.DB_PORT, 10) || 1433,
+    //   options: {
+    //     encrypt: false,
+    //   },
+    // },
+
+    // For a async connection (forRootAsync)
+    type: process.env.DB_CLIENT,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT) | 1433,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: false,
+    entities: ['dist/**/**/*.entity{.ts,.js}'],
+    options: {
+      encrypt: false,
     },
   },
 });
--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './strategy/local.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtStrategy } from './strategy/jwt.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 // import { UsersRepository } from '../users/users.repository';
 
 @Module({
