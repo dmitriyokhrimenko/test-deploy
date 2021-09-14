d8e8be8c530a817f05e22aae90b02a8046d77e84
dm.okhrimenko <dm.okhrimenko@gmail.com>
2021-09-14 19:13:12 +0300

    infra(tf): nat gateway

--- infrastructure/terraform/modules/instances/instances.tf
+++ infrastructure/terraform/modules/instances/instances.tf
@@ -12,7 +12,18 @@ resource "aws_instance" "app-instance" {
   subnet_id = "${var.vpc_private_subnet}"
   key_name = "${var.key_name}"
   vpc_security_group_ids = ["${var.security_group_id}"]
+
 }
+
+resource "local_file" "hosts_ini" {
+  content = templatefile("/Users/easternpeak/www/ryde-core/infrastructure/ansible/templates/hosts.tpl",
+  {
+    host = aws_instance.app-instance.private_ip
+  }
+  )
+  filename = "/Users/easternpeak/www/ryde-core/infrastructure/ansible/hosts.ini"
+}
+
 resource "aws_instance" "jumpbox" {
   ami = "${lookup(var.aws_ubuntu_awis,var.region)}"
   instance_type = "t2.micro"
@@ -31,25 +42,45 @@ resource "aws_eip" "jumpbox-eip" {
   network_interface         = aws_instance.jumpbox.primary_network_interface_id
   associate_with_private_ip = aws_instance.jumpbox.private_ip
 
-  connection {
-    type = "ssh"
+//  connection {
+//    type = "ssh"
+//    user = var.ec2_user
+//    host = aws_eip.jumpbox-eip.public_ip
+//    private_key = file(var.ec2_instance_private_key_path)
+//  }
+
+//  provisioner "file" {
+//    source      = "${var.ec2_instance_private_key_path}"
+//    destination = "/home/ubuntu/sshKeyRyde.pem"
+//  }
+
+//  provisioner "file" {
+//    source      = "/Users/easternpeak/www/ryde-core/infrastructure/ansible"
+//    destination = "/home/ubuntu"
+//  }
+
+//  provisioner "remote-exec" {
+//    inline = [
+//      "sudo apt update",
+//      "sudo apt install software-properties-common -y",
+//      "sudo add-apt-repository --yes --update ppa:ansible/ansible -y",
+//      "sudo apt install ansible -y",
+//      "ansible-galaxy install -r /home/ubuntu/ansible/requirements.yml",
+//      "ansible-playbook -u ${var.ec2_user} -i 'ansible/hosts.ini' ansible/provision.yml"
+//    ]
+//  }
+}
+
+resource "local_file" "ssh_config" {
+  content = templatefile("/Users/easternpeak/www/ryde-core/infrastructure/terraform/templates/ssh_config.tpl",
+  {
+    bastion_host = aws_eip.jumpbox-eip.public_ip,
     user = var.ec2_user
-    host = aws_eip.jumpbox-eip.public_ip
-    private_key = file(var.ec2_instance_private_key_path)
   }
+  )
+  filename = "/Users/easternpeak/.ssh/aws"
 
-  provisioner "file" {
-    source      = "/Users/easternpeak/www/ryde-core/infrastructure/ansible"
-    destination = "/home/ubuntu"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update",
-      "sudo apt install software-properties-common -y",
-      "sudo add-apt-repository --yes --update ppa:ansible/ansible -y",
-      "sudo apt install ansible -y",
-      "ansible-playbook -u ${var.ec2_user} -i 'ansible/hosts.ini' --private-key ${var.ec2_instance_private_key_path} ansible/provision.yml"
-    ]
+  provisioner "local-exec" {
+    command = "ansible-playbook -u ${var.ec2_user} -i 'ansible/hosts.ini' /Users/easternpeak/www/ryde-core/infrastructure/ansible/provision.yml"
   }
 }
